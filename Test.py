e5a11935557f8455a679a0dd5c945367ea108838
TOMEK <tomekrajzer@gmail.com>
2017-10-02 22:05:33 +0200

    Add logging, format dict before saving

--- Test.py
+++ Test.py
@@ -1,13 +1,37 @@
-from GetConfig import GetConfigParam
 import requests
+import logging
+
+from GetConfig import GetConfigParam
+
+logging.basicConfig(filename='WeatherData.log', level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
 CONFIG_FILENAME = 'config.cfg'
 
 
 def get_weather_data(**data):
 	url = data['url'] + '/data/2.5/weather?q=' + data['city'] + '&' + 'units=' + data['units'] + '&' + 'appid=' + data['api_key']
 
-	r = requests.post(url)
-	print(str(r.json()))
+	response = requests.post(url)
+	response_json = response.json()
+	logging.debug(str(response_json))
+	return {
+		'cloudiness': response_json['clouds']['all'],
+		'city': response_json['name'],
+		'humidity': response_json['main']['humidity'],
+		'pressure': response_json['main']['pressure'],
+		'temp': response_json['main']['temp'],
+		'sunrise': response_json['sys']['sunrise'],
+		'sunset': response_json['sys']['sunset'],
+		'weather_description': response_json['weather'][0]['description'],
+		'rain': response_json['rain']['3h'] if 'rain' in response_json else 0,
+		'snow': response_json['snow']['3h'] if 'snow' in response_json else 0,
+		'weather_description': response_json['weather'][0]['description'],
+		'wind_speed': response_json['wind']['speed'],
+		'wind_deg': response_json['wind']['deg'],
+		'visibility': response_json['visibility'],
+
+	}
 
 
 def get_config_param(cfg):
@@ -21,4 +45,5 @@ def get_config_param(cfg):
 if __name__ == '__main__':
 	cfg = GetConfigParam(CONFIG_FILENAME)
 	config_param = get_config_param(cfg)
-	get_weather_data(**config_param)
+	x = get_weather_data(**config_param)
+	print(str(x))
